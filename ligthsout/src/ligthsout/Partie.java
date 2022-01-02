c4d0e49284638fd4699f5777f1170bfd70fe4afe
theob <theob@LAPTOP-1J4LJDK2>
2022-01-02 16:48:37 +0100

    rendu tp

--- ligthsout/src/ligthsout/Partie.java
+++ ligthsout/src/ligthsout/Partie.java
@@ -4,10 +4,29 @@
  */
 package ligthsout;
 
+import java.util.Scanner;
+
 /**
  *
  * @author theob
  */
 public class Partie {
+     
+    public static void main(String[] args){
+        
+     Scanner sc = new Scanner(System.in);      
+        
+        System.out.println("veuillez entrer 1 pour facile et 2 pour difficile");
+        int niveaudifficulté = sc.nextInt();
+       
+     if(niveaudifficulté==1){
+        Lightsout jeu = new Lightsout(5);  
+        jeu.debutpartie();
+        jeu.afficherGrilleFacile();
+        
+        int compteur;
+        compteur = 0;
+      }
+    }
     
 }
