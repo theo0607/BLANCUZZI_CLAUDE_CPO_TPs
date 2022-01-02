13c9d224b7a56d3fa2c859dc238e04e7802a3e1f
theob <theob@LAPTOP-1J4LJDK2>
2022-01-02 14:11:10 +0100

    rendu tp

--- ligthsout/src/ligthsout/Lightsout.java
+++ ligthsout/src/ligthsout/Lightsout.java
@@ -10,4 +10,17 @@ package ligthsout;
  */
 public class Lightsout {
     
+    public boolean [ ][ ] grille;
+    
+    public Lightsout(int taillegrille){
+      
+      grille = new boolean [taillegrille][taillegrille];
+      
+      for (int i=0 ; i<taillegrille; i++){
+          for (int j=0 ; j<taillegrille; j++){
+              grille[i][j] = false;
+          }
+      }
+   }
+    
 }
